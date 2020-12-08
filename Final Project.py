c8ef98986df8b86f674aade58619054fdce1bcc7
张宸霖 <55804757+zchenlin@users.noreply.github.com>
2020-12-07 20:25:05 -0500

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -3,17 +3,19 @@ import unittest
 import os
 import requests
 import sqlite3
+import matplotlib
+import matplotlib.pyplot as plt 
 
 api_key = "aa22fc25508e6ebea46f7992e6395391"
 
 def request_url(company_name):
-    url = "https://financialmodelingprep.com/api/v3/income-statement/{}?period=quarter&limit=3&apikey=7a45eead1c6d464025fc42c7d0a4bf2e"
+    url = "https://financialmodelingprep.com/api/v3/income-statement/{}?period=quarter&limit=2&apikey=a9cab2ec0ba9cc37205ce27eaa565bbb"
     final_url = url.format(company_name)
     return final_url 
 
 def get_etf_company(etf): 
     result = [] 
-    url = "https://financialmodelingprep.com/api/v3/etf-holder/{}?apikey=7a45eead1c6d464025fc42c7d0a4bf2e"
+    url = "https://financialmodelingprep.com/api/v3/etf-holder/{}?apikey=a9cab2ec0ba9cc37205ce27eaa565bbb"
     final_url = url.format(etf)
     r = requests.get(final_url)
     try:
@@ -31,16 +33,12 @@ def get_data_json(company_name):
     url = request_url(company_name)
     r = requests.get(url)
     k = json.loads(r.text)  
-    try: 
-        result = []
-        for i in k: 
-            info = {} 
-            info["quarter"] = i["date"]
-            info["revenue"] = i["revenue"]
-            info['grossProfit'] = i["grossProfit"]
-            info['grossProfitRatio'] = i["grossProfitRatio"]
-            result.append(info)
-        return result 
+#    print(k)
+    try:  
+        info = {} 
+        info["grossProfit_quarter_1"] = k[0]['grossProfitRatio']
+        info["grossProfit_quarter_2"] = k[1]['grossProfitRatio']
+        return info 
     except:
         print("Error")
         return None 
@@ -51,7 +49,7 @@ def setupDatabase(file_name):
     cur = conn.cursor()
     return cur, conn
 
-def setupCompanyTable(company_list, cur, conn):
+"""def setupCompanyTable(company_list, cur, conn):
     id = 1
     cur.execute('DROP TABLE IF EXISTS Companies')
     cur.execute('CREATE TABLE Companies (company_id INTEGER PRIMARY KEY, name TEXT)') 
@@ -60,40 +58,46 @@ def setupCompanyTable(company_list, cur, conn):
         name = company 
         cur.execute("INSERT INTO Companies (company_id, name) VALUES (?,?)", (comapny_id, name)) 
         id = id + 1 
-        conn.commit() 
+        conn.commit()"""
 
 
 def setupIncomeDataTable(json_data, company_list, cur, conn):
     cur.execute('DROP TABLE IF EXISTS IncomeData')
-    cur.execute('CREATE TABLE IncomeData (company_id integer, quarter TEXT, revenue INTEGER, grossProfit INTEGER, grossProfitRatio REAL)')
-    number = 1 
+    cur.execute('CREATE TABLE IncomeData (company_name TEXT PRIMARY KEY, grossProfitRatio_quarter_1 REAL, grossProfitRatio_quarter_2 REAL )')
     for company in company_list: 
-        for data in json_data[company]: 
-            cur.execute("SELECT company_id FROM Companies WHERE name = ?", (company,))
-            company_id = cur.fetchone()[0] 
-            quarter = data['quarter']
-            revenue = data['revenue']
-            grossProfit = data['grossProfit']
-            grossProfitRatio = data['grossProfitRatio']
-            cur.execute('INSERT INTO IncomeData (company_id, quarter, revenue, grossProfit, grossProfitRatio) VALUES (?,?,?,?,?)', (company_id, quarter, revenue, grossProfit, grossProfitRatio))
-            conn.commit() 
-        number = number + 1 
+        ratio_1 = json_data[company]['grossProfit_quarter_1']
+        ratio_2 = json_data[company]['grossProfit_quarter_2']
+        cur.execute('INSERT INTO IncomeData (company_name, grossProfitRatio_quarter_1, grossProfitRatio_quarter_2) VALUES (?,?,?)', (company, ratio_1, ratio_2))
+        conn.commit() 
+
+def dataCalculation(cur, conn):
+    result = [] 
+    cur.execute('SELECT grossProfitRatio_quarter_1, grossProfitRatio_quarter_2 FROM IncomeData')
+    for row in cur:
+        diff = row[0] - row[1] 
+        result.append(diff)
+    return result 
+
+def visualization(data):
+    plt.hist(data, range=[-1,1], density=1, bins=20, edgecolor = 'black')
+    plt.xlabel('GrossProfitRatio Difference')
+    plt.ylabel('Probability')
+    plt.show() 
         
-         
 def main(): 
-    count = 0
     final_data = {}
-    eft = "XRT"
+    eft = "SPY"
     company_names = get_etf_company(eft)
     for company_name in company_names: 
         a = get_data_json(company_name)
-        final_data[company_name] = a
-    json_data = json.dumps(final_data)
+        if a != None:
+            final_data[company_name] = a
+    final_data_keys = final_data.keys()
     cur, conn = setupDatabase('Companies.db')
-    setupCompanyTable(company_names, cur, conn)
-    setupIncomeDataTable(final_data, company_names, cur, conn)
-
+    setupIncomeDataTable(final_data, final_data_keys, cur, conn)
+    visualization_data = dataCalculation(cur,conn)
     conn.close() 
+    visualization(visualization_data)
 
     
  
